3a19e5df312e8fe8bf389aca5e8499c93f5f7cfa
Rohit Ranjan <rrohit2005@gmail.com>
2025-02-23 19:37:30 +0530

    frontend section

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,12 +1,24 @@
+import { LandPlot } from 'lucide-react';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
+function Home() {
+  return <div className="text-center text-blue-500 p-4">Home Page</div>;
+}
+
+function About() {
+  return <div className="text-center text-blue-500 p-4">About Page</div>;
+}
 
 function App() {
   return (
-    <>
-      <div className="text-center text-blue-500 p-4">
-        Hi
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
