2e900e28597447275b4ac14298594045b31db07e
Rohit Ranjan <rrohit2005@gmail.com>
2025-02-23 16:31:43 +0530

    frontend-tailwind

---
+++ project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-blue-500 p-4">
+        Hi
+      </div>
+    </>
+  );
+}
+
+export default App;
